b1cc2c400e0cd29f2278bc6e1a221a3343bbd7c8
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-07 16:37:03 +0100

    Add jwt dependency

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -22,4 +22,10 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  'prettier/prettier': [
+    'error',
+    {
+      endOfLine: 'auto',
+    },
+  ],
 };
