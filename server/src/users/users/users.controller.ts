28b4542ede6ecc3bd04f80e6105cddf64979c2fd
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-07 16:28:13 +0100

    Creare users module

---
+++ server/src/users/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/utils/guards/auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor() {}
+
+  @UseGuards(AuthGuard)
+  @Get()
+  getUsers() {
+    return 'This action returns all users';
+  }
+
+  // create user and send email contains generated password
+  createUser() {
+    return 'This action adds a new user';
+  }
+
+  updateUser() {
+    return 'This action updates a user';
+  }
+
+  removeUser() {
+    return 'This action removes a user';
+  }
+}
