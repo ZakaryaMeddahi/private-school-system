54bf27e3c7f5c65d3f1e3c4747eec46449aa2a6b
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-13 23:10:17 +0100

    Add topics service

--- server/src/modules/courses/courses.service.ts
+++ server/src/modules/courses/courses.service.ts
@@ -3,29 +3,52 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Course } from 'src/shared/entities/course.entity';
 import { CreateCourseParams, UpdateCourseParams } from 'src/shared/types';
 import { Repository } from 'typeorm';
+import { TopicsService } from './topics/topics.service';
 
 @Injectable()
 export class CoursesService {
   constructor(
     @InjectRepository(Course)
     private readonly coursesRepository: Repository<Course>,
+    private readonly topicsService: TopicsService,
   ) {}
 
   async findAll() {
     try {
-      const courses = await this.coursesRepository.find();
+      const courses = await this.coursesRepository.find({
+        relations: ['topics'],
+      });
       return courses;
     } catch (error) {
       throw new HttpException('Cannot get courses', 500);
     }
   }
 
+  async findOne(id: number) {
+    try {
+      const course = await this.coursesRepository.findOne({
+        where: { id },
+        relations: ['topics'],
+      });
+
+      if (!course) return null;
+
+      return course;
+    } catch (error) {
+      throw new HttpException('Cannot get course', 500);
+    }
+  }
+
   async create(courseData: CreateCourseParams) {
     try {
-      const { topics, ...course } = courseData;
-      console.log(course);
+      const { topics } = courseData;
 
-      const newCourse = this.coursesRepository.create(course);
+      if (topics) {
+        const newTopics = await this.topicsService.createMany(topics);
+        courseData.topics = newTopics;
+      }
+
+      const newCourse = this.coursesRepository.create(courseData);
       return this.coursesRepository.save(newCourse);
     } catch (error) {
       console.error(error);
@@ -39,13 +62,14 @@ export class CoursesService {
 
       let course = await this.coursesRepository.findOne({ where: { id } });
 
-      console.log(course);
-
       if (!course) return null;
 
       course = { ...course, ...updatedCourse };
 
-      console.log(course);
+      if (topics) {
+        const updatedTopics = await this.topicsService.updateMany(topics);
+        course.topics = updatedTopics;
+      }
 
       return this.coursesRepository.save(course);
     } catch (error) {
