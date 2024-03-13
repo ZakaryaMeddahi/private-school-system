a5531d81ef3fc616e4705572a3cbbc7aced236b1
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-13 12:15:29 +0100

    Add api versioning

--- server/src/modules/chats/chats.controller.ts
+++ server/src/modules/chats/chats.controller.ts
@@ -1,4 +1,4 @@
 import { Controller } from '@nestjs/common';
 
-@Controller('chats')
+@Controller('api/v1/chats')
 export class ChatsController {}
--- server/src/modules/rooms/rooms.controller.ts
+++ server/src/modules/rooms/rooms.controller.ts
@@ -1,4 +1,4 @@
 import { Controller } from '@nestjs/common';
 
-@Controller('rooms')
+@Controller('api/v1/rooms')
 export class RoomsController {}
