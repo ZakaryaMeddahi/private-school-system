95b73b367e0365fa80f19c33b3786b73b547eb6d
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-13 23:05:13 +0100

    Setup chat and messages routes

--- server/src/modules/chats/chats.controller.ts
+++ server/src/modules/chats/chats.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, Patch } from '@nestjs/common';
+import { UpdateChatDto } from './dto/updateChat.dto';
+import { ChatsService } from './chats.service';
 
-@Controller('api/v1/chats')
-export class ChatsController {}
+@Controller('api/v1/course/:courseId/chats')
+export class ChatsController {
+  constructor(private readonly chatsService: ChatsService) {}
+
+  @Get()
+  async getChats(id: number) {
+    try {
+      const chats = await this.chatsService.findAll();
+
+      return {
+        status: 'success',
+        message: 'Loaded chats successfully',
+        data: chats,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(error.response, error.status);
+    }
+  }
+
+  @Patch(':id')
+  async updateChat(id: number, chatData: UpdateChatDto) {
+    try {
+      const updatedChat = await this.chatsService.update(id, chatData);
+
+      if (!updatedChat) {
+        throw new HttpException('Chat not found', 404);
+      }
+
+      return {
+        status: 'success',
+        message: 'Chat updated successfully',
+        data: updatedChat,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(error.response, error.status);
+    }
+  }
+}
--- server/src/modules/chats/chats.service.ts
+++ server/src/modules/chats/chats.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Chat } from 'src/shared/entities/chat.entity';
+import { Repository } from 'typeorm';
+import { UpdateChatParams } from 'src/shared/types';
 
 @Injectable()
-export class ChatsService {}
+export class ChatsService {
+  constructor(
+    @InjectRepository(Chat) private readonly chatsRepository: Repository<Chat>,
+  ) {}
+
+  async findAll() {
+    try {
+      const chats = await this.chatsRepository.find();
+
+      return chats;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Cannot get chats', 500);
+    }
+  }
+
+  async update(id: number, chatData: UpdateChatParams) {
+    try {
+      const chat = await this.chatsRepository.findOne({ where: { id } });
+
+      if (!chat) null;
+
+      const updatedChat = await this.chatsRepository.save({
+        ...chat,
+        ...chatData,
+      });
+
+      return updatedChat;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Cannot update chat', 500);
+    }
+  }
+}
