f2dd5b93bd0a2c62f818b859a2745499945f2020
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-15 18:00:23 +0100

    Implement AuthUser decorator

---
+++ server/src/decorators/user.decorator.ts
@@ -0,0 +1,8 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+export const AuthUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
