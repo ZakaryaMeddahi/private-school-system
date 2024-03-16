f178618cb35f1f988ff3a6b649e8d9d3b315f4f3
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-16 06:02:18 +0100

    Add validation into dtos

--- server/src/modules/courses/dto/createCourse.dto.ts
+++ server/src/modules/courses/dto/createCourse.dto.ts
@@ -1,13 +1,33 @@
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Difficulty, DurationUnit } from 'src/shared/enums';
 
 export class createCourseDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
   description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
+
+  @IsNotEmpty()
+  @IsString()
   language: string;
+
+  @IsEnum(Difficulty, { message: 'Invalid difficulty Level' })
   difficulty: Difficulty;
+
+  @IsNumber()
   enrollmentLimit: number;
+
+  @IsNotEmpty()
+  @IsNumber()
   duration: number;
+
+  @IsEnum(DurationUnit, { message: 'Invalid duration unit' })
   durationUnit: DurationUnit;
+
   topics: [];
 }
