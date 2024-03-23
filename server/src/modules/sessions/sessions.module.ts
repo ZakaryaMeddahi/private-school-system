602e6bb16934170e20bd8f12c48a333d96e92ff6
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-23 12:53:42 +0100

    Update imports

--- server/src/modules/sessions/sessions.module.ts
+++ server/src/modules/sessions/sessions.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SessionsService } from './sessions.service';
-import { SessionsGateway } from './sessions.gateway';
 
 @Module({
-  providers: [SessionsService, SessionsGateway]
+  providers: [SessionsService]
 })
 export class SessionsModule {}
