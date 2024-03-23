c686131f60dcb909f734d3ed809cdd523536e2ab
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-23 12:51:51 +0100

    Update imoorts

--- server/src/modules/chats/chats.module.ts
+++ server/src/modules/chats/chats.module.ts
@@ -3,10 +3,11 @@ import { ChatsController } from './chats.controller';
 import { ChatsService } from './chats.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from 'src/shared/entities/chat.entity';
+import { ChatsGateway } from './chats.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Chat])],
   controllers: [ChatsController],
-  providers: [ChatsService],
+  providers: [ChatsService, ChatsGateway],
 })
 export class ChatsModule {}
