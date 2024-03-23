2fc2e36838e7a6812305311c2a1f7ee21ac7acfa
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-23 12:48:14 +0100

    Add get chat controller

--- server/src/modules/chats/chats.controller.ts
+++ server/src/modules/chats/chats.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, HttpException, Patch } from '@nestjs/common';
 import { UpdateChatDto } from './dto/updateChat.dto';
 import { ChatsService } from './chats.service';
 
-@Controller('api/v1/course/:courseId/chats')
+@Controller('api/v1/courses/:courseId/chats')
 export class ChatsController {
   constructor(private readonly chatsService: ChatsService) {}
 
@@ -15,6 +15,25 @@ export class ChatsController {
         status: 'success',
         message: 'Loaded chats successfully',
         data: chats,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(error.response, error.status);
+    }
+  }
+
+  async getChat(id: number) {
+    try {
+      const chat = await this.chatsService.findOne(id);
+
+      if (!chat) {
+        throw new HttpException('Chat not found', 404);
+      }
+
+      return {
+        status: 'success',
+        message: 'Chat loaded successfully',
+        data: chat,
       };
     } catch (error) {
       console.error(error);
