71642849b11a9b949b448eb037f768b4f6624102
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-31 00:46:20 +0100

    Create mail module

---
+++ server/src/modules/mail/mail.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        auth: {
+          user: 'zakaryameddahi@gmail.com',
+          pass: 'sntz mcab fdzx tvnu',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <zakaryameddahi@gmail.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
