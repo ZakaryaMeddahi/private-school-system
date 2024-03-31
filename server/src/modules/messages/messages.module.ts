c5ae084c5a525e04508846b4f1b7a53fa3e57a85
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-31 10:32:46 +0100

    Add FilesModule and ChatsModule into imports

--- server/src/modules/messages/messages.module.ts
+++ server/src/modules/messages/messages.module.ts
@@ -3,10 +3,12 @@ import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from 'src/shared/entities/message.entity';
+import { FilesModule } from '../files/files.module';
+import { ChatsModule } from '../chats/chats.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
-  providers: [MessagesService],
+  imports: [TypeOrmModule.forFeature([Message]), FilesModule, ChatsModule],
   controllers: [MessagesController],
+  providers: [MessagesService],
 })
 export class MessagesModule {}
