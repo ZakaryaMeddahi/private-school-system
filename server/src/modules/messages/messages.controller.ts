3c0713057cbef2df230d078e2135185cfe7227fc
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-31 10:34:41 +0100

    Implement create message routes

--- server/src/modules/messages/messages.controller.ts
+++ server/src/modules/messages/messages.controller.ts
@@ -1,16 +1,30 @@
 import {
+  Body,
   Controller,
   Get,
   HttpException,
   NotFoundException,
   Param,
+  ParseIntPipe,
+  Post,
   Query,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { MessagesService } from './messages.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { v2 as cloudinary } from 'cloudinary';
+import { uploadFile } from 'src/helpers/object-storage';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { FilesService } from '../files/files.service';
 
 @Controller('api/v1')
 export class MessagesController {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    private readonly filesService: FilesService,
+  ) {}
 
   @Get('courses/:courseId/chats/:chatId/messages')
   getMessagesForChat(
@@ -71,6 +85,72 @@ export class MessagesController {
     } catch (error) {
       console.error(error);
       throw new HttpException('Cannot get messages', 500);
+    }
+  }
+
+  @Post('courses/:courseId/chats/:chatId/messages')
+  @UseInterceptors(FileInterceptor('file'))
+  async sendFileInChat(
+    @Param('chatId') chatId: number,
+    @Body() messageData: CreateMessageDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    try {
+      const newFile = await this.filesService.create(file);
+
+      const newMessage = await this.messagesService.createByChatId(chatId, {
+        ...messageData,
+        file: newFile,
+      });
+
+      console.log('--------- File Message (Chat) ---------');
+      console.log(newMessage);
+      console.log('---------------------------------------');
+
+      // TODO: Send message to the room
+
+      return {
+        status: 'success',
+        message: 'Message sent successfully',
+        data: newMessage,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Cannot upload file', 500);
+    }
+  }
+
+  @Post('courses/:courseId/rooms/:roomId/messages')
+  @UseInterceptors(FileInterceptor('file'))
+  async sendFileInRoom(
+    @Param('roomId', ParseIntPipe) roomId: number,
+    @Body() messageData: CreateMessageDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    try {
+      // Create file entity and save it in the database with relation to the message
+      const newFile = await this.filesService.create(file);
+
+      // Create message
+      const newMessage = await this.messagesService.createByRoomId(roomId, {
+        ...messageData,
+        file: newFile,
+      });
+
+      console.log('--------- File Message (Room) ---------');
+      console.log(newMessage);
+      console.log('---------------------------------------');
+
+      // TODO: Send message to the room
+
+      return {
+        status: 'success',
+        message: 'Message Sent successfully',
+        data: newMessage,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Cannot upload file', 500);
     }
   }
 }
