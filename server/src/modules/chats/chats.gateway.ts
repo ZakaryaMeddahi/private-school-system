52743c697e32df677a7d2b0925e4dc60e8c749b9
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-31 10:42:18 +0100

    Implement chats gateway

--- server/src/modules/chats/chats.gateway.ts
+++ server/src/modules/chats/chats.gateway.ts
@@ -1,10 +1,18 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
+  WsException,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { MessagesService } from '../messages/messages.service';
+import { ChatsService } from './chats.service';
+import { NotFoundException } from '@nestjs/common';
+
+// We should have as DS like Map to store user sockets in chat
 
 @WebSocketGateway({
   namespace: 'chats',
@@ -12,12 +20,80 @@ import { Socket } from 'socket.io';
     origin: 'http://localhost:5000',
   },
 })
-export class ChatsGateway {
+export class ChatsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly chatsService: ChatsService,
+    private readonly messagesService: MessagesService,
+  ) {}
+  // Event: send message
   @SubscribeMessage('message')
-  handleMessage(
+  async handleMessage(
     @MessageBody() data: any,
     @ConnectedSocket() client: Socket,
-  ): string {
-    return 'Hello world!';
+  ): Promise<any> {
+    try {
+      console.log(data);
+      // const { id, firstName, lastName, role, profilePicture } = user
+      const { chatId, message } = data;
+      // check if chat exist
+      const chat = await this.chatsService.findOne(chatId);
+      console.log(chat);
+      if (!chat) {
+        throw new WsException(`There is no chat with id ${chatId}`);
+      }
+      // Implement creating message by chat id
+      const newMessage = await this.messagesService.createByChatId(chatId, message);
+      // Broadcast message to all users in chat
+      client.to(`chat-${data.chatId}`).emit('message', newMessage);
+      return newMessage;
+    } catch (error) {
+      console.error(error);
+      return { status: 'error', message: error.message };
+    }
+  }
+
+  // join socket.io room
+  @SubscribeMessage('join-room')
+  async joinRoom(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+    try {
+      const { chatId } = data;
+      // check if chat exist
+      const chat = await this.chatsService.findOne(chatId);
+      console.log(chat);
+      if (!chat) {
+        throw new WsException(`There is no chat with id ${chatId}`);
+      }
+      // TODO: check if the user has access to the chat
+      client.join(`chat-${chatId}`);
+      return { result: `User with id ${client.id} joined the chat ${chatId}` };
+    } catch (error) {
+      console.error(error);
+      return { status: 'error', message: error.message };
+    }
+  }
+
+  // leave socket.io room
+  @SubscribeMessage('leave-room')
+  leaveRoom(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+    const { chatId } = data;
+    client.leave(`chat-${chatId}`);
+    return { result: `User with id ${client.id} left the chat ${chatId}` };
+  }
+
+  // Event: connection
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('------------------------------------------------');
+    console.log(`Client with id ${client.id} connected`);
+    console.log('------------------------------------------------');
+  }
+
+  // Event: disconnection
+  handleDisconnect(client: Socket) {
+    console.log('------------------------------------------------');
+    console.log(`Client with id ${client.id} disconnected`);
+    console.log('------------------------------------------------');
+    client.rooms.forEach((room) => {
+      client.leave(room);
+    });
   }
 }
