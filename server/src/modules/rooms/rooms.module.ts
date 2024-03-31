bff2ad4c859c2c63c365994549aebccdc8689249
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-31 12:01:26 +0100

    Add Message and MessagesService as dependencies

--- server/src/modules/rooms/rooms.module.ts
+++ server/src/modules/rooms/rooms.module.ts
@@ -6,10 +6,12 @@ import { Room } from 'src/shared/entities/room.entity';
 import { RoomsGateway } from './rooms.gateway';
 import { ChatsService } from '../chats/chats.service';
 import { Chat } from 'src/shared/entities/chat.entity';
+import { Message } from 'src/shared/entities/message.entity';
+import { MessagesService } from '../messages/messages.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room, Chat])],
+  imports: [TypeOrmModule.forFeature([Room, Chat, Message])],
   controllers: [RoomsController],
-  providers: [RoomsService, RoomsGateway, ChatsService],
+  providers: [RoomsService, RoomsGateway, ChatsService, MessagesService],
 })
 export class RoomsModule {}
