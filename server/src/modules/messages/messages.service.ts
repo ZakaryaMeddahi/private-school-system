530d4c92e8182ddf0ce21265cc99ebf38661d439
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-31 10:31:33 +0100

    Implement createByChatId and createbyRoomId

--- server/src/modules/messages/messages.service.ts
+++ server/src/modules/messages/messages.service.ts
@@ -1,14 +1,16 @@
 import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Message } from 'src/shared/entities/message.entity';
-import { MessagesOptions } from 'src/shared/types';
+import { CreateMessageParams, MessagesOptions } from 'src/shared/types';
 import { Repository } from 'typeorm';
+import { ChatsService } from '../chats/chats.service';
 
 @Injectable()
 export class MessagesService {
   constructor(
     @InjectRepository(Message)
     private readonly messagesRepository: Repository<Message>,
+    private readonly chatsService: ChatsService,
   ) {}
 
   async findByChatId(options: MessagesOptions) {
@@ -44,6 +46,45 @@ export class MessagesService {
     } catch (error) {
       console.error(error);
       throw new HttpException('Cannot get messages', 500);
+    }
+  }
+
+  // Implement create message
+  async createByChatId(chatId: number, MessageData: CreateMessageParams) {
+    try {
+      const newMessage = this.messagesRepository.create({
+        chat: { id: chatId },
+        content: MessageData.content,
+        file: MessageData.file || null,
+      });
+
+      const messageEntity = await this.messagesRepository.save(newMessage);
+
+      return messageEntity;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Cannot create message', 500);
+    }
+  }
+
+  async createByRoomId(roomId: number, MessageData: CreateMessageParams) {
+    try {
+      const chat = await this.chatsService.findByRoomId(roomId);
+
+      const newMessage = this.messagesRepository.create({
+        chat: { id: chat.id },
+        content: MessageData.content,
+        file: MessageData.file || null,
+      });
+
+      console.log('inside create by room id');
+
+      const messageEntity = await this.messagesRepository.save(newMessage);
+
+      return messageEntity;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Cannot create message', 500);
     }
   }
 }
