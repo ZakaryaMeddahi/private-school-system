23f5960ce5561266a734e73f3495f28e362ae487
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-04-01 16:23:33 +0100

    Implement startSession and endSession methods

--- server/src/modules/rooms/rooms.gateway.ts
+++ server/src/modules/rooms/rooms.gateway.ts
@@ -9,6 +9,9 @@ import { Socket } from 'socket.io';
 import { MessagesService } from '../messages/messages.service';
 import { RoomsService } from './rooms.service';
 import { ChatsService } from '../chats/chats.service';
+import { SessionsService } from '../sessions/sessions.service';
+import { StudentSessionsService } from '../student-sessions/student-sessions.service';
+import { TopicsService } from '../topics/topics.service';
 
 // We should have as DS like Map to store user sockets in room
 
@@ -23,6 +26,9 @@ export class RoomsGateway {
     private readonly roomsService: RoomsService,
     private readonly chatsService: ChatsService,
     private readonly messagesService: MessagesService,
+    private readonly topicsService: TopicsService,
+    private readonly sessionsService: SessionsService,
+    private readonly studentSessionsService: StudentSessionsService,
   ) {}
   // Event: send message
   @SubscribeMessage('message')
@@ -130,6 +136,9 @@ export class RoomsGateway {
       // Join room
       client.join(`room-${roomId}`);
 
+      // Broadcast Message to all users in room
+      client.to(`room-${roomId}`).emit('user-joined', { socketId: client.id });
+
       return {
         result: `User with id ${client.id} joined the room ${roomId}`,
       };
@@ -147,8 +156,73 @@ export class RoomsGateway {
     // Leave room
     client.leave(`room-${roomId}`);
 
+    // Broadcast Message to all users in room
+    client.to(`room-${roomId}`).emit('user-left', { socketId: client.id });
+
     return { result: `User with id ${client.id} left the chat ${roomId}` };
   }
+
+  // TODO: Event -> start session (use create session from sessions service)
+  @SubscribeMessage('start-session')
+  async startSession(
+    @MessageBody() data: any,
+    @ConnectedSocket() client: Socket,
+  ) {
+    try {
+      const { roomId, topicId } = data;
+      const topic = await this.topicsService.findOne(topicId);
+
+      const session = await this.sessionsService.create({
+        agoraChannel: `${topic.title}-${topicId}`,
+        agoraToken: 'agora-token',
+      });
+
+      await this.topicsService.updateOne(topicId, session.id, {});
+
+      // Broadcast message to all users in chat
+      client.to(`room-${roomId}`).emit('session-started', { session });
+
+      return {
+        status: 'success',
+        result: 'Session started successfully',
+        session,
+      };
+    } catch (error) {
+      console.error(error);
+      return { status: 'error', result: error.message };
+    }
+  }
+  // TODO: Event -> end session (use update session from sessions service)
+  @SubscribeMessage('end-session')
+  async endSession(
+    @MessageBody() data: any,
+    @ConnectedSocket() client: Socket,
+  ) {
+    try {
+      const { sessionId, roomId } = data;
+
+      console.log(sessionId);
+
+      const session = await this.sessionsService.update(sessionId, {
+        endTime: new Date(),
+      });
+
+      // Broadcast message to all users in chat
+      client.to(`room-${roomId}`).emit('session-ended', { session });
+
+      return {
+        status: 'success',
+        result: 'Session ended successfully',
+        session,
+      };
+    } catch (error) {
+      console.error(error);
+      return { status: 'error', result: error.message };
+    }
+  }
+
+  // TODO: Event -> join session (use create session from student-sessions service)
+  // TODO: Event -> leave session (use update session from student-sessions service)
 
   // Event: connection
   handleConnection(client: Socket, ...args: any[]) {
@@ -168,7 +242,4 @@ export class RoomsGateway {
       client.leave(room);
     });
   }
-
-  // TODO: Event -> start session (use create session from sessions service)
-  // TODO: Event -> end session (use update session from sessions service)
 }
