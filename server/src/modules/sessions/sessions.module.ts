75937b06b9fe82bec51a1b324c4c97a1a6ce8a14
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-04-01 15:54:14 +0100

    Add SessionsService to exports array

--- server/src/modules/sessions/sessions.module.ts
+++ server/src/modules/sessions/sessions.module.ts
@@ -6,5 +6,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [TypeOrmModule.forFeature([Session])],
   providers: [SessionsService],
+  exports: [SessionsService]
 })
 export class SessionsModule {}
