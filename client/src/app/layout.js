6b5ad7b22026f6032992ab7f274fb3cb1fee0d24
l3alisidahmed <sidoabdelali06@gmail.com>
2024-04-04 18:38:09 +0100

    add Poppins font

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/Nav Bar/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
