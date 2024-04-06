8c4fa194fbad5c85db472b82dbc85f37e05f6da1
l3alisidahmed <sidoabdelali06@gmail.com>
2024-04-06 17:12:28 +0100

    add props

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 import { Wrap, WrapItem, Image, Heading } from '@chakra-ui/react';
 
-const Logo = () => {
+const Logo = ({ boxSize, fontSize }) => {
     return (
         <Wrap spacing={0}>
             <WrapItem alignItems='center'>
-                <Image boxSize='60px' src='/1-removebg-preview.png' />
+                <Image boxSize={boxSize || '60px'} src='/1-removebg-preview.png' />
             </WrapItem>
             <WrapItem alignItems='center'>
-                <Heading as='h1' size='lg' fontSize='24px'>Education</Heading>
+                <Heading as='h1' size='lg' fontSize={ fontSize || '24px'} color="white">Education</Heading>
             </WrapItem>
         </Wrap>
     );
