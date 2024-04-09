408d761f24d6ba48a8e1ee0fd96f1054ffceb9ed
l3alisidahmed <sidoabdelali06@gmail.com>
2024-04-09 18:46:47 +0100

    create a row component for contact page

---
+++ client/src/components/Row Input/RowInput.jsx
@@ -0,0 +1,15 @@
+import {
+    Box,
+} from '@chakra-ui/react';
+import ContactInput from '../Contact Input/ContactInput';
+
+const RowInput = ({ label_I1, type_I1, placeholder_I1, label_I2, type_I2, placeholder_I2 }) => {
+    return (
+        <Box display='flex' flexDir='row' width='100%' gap='10' marginTop='40px'>
+            <ContactInput label={label_I1} type={type_I1 || 'text'} placeholder={ placeholder_I1 || ''} />
+            <ContactInput label={label_I2} type={type_I2 || 'text'} placeholder={ placeholder_I2 || ''} />
+        </Box>
+    );
+}
+
+export default RowInput;
