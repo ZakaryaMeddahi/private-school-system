a788ef7d48e63b4efee87e87c6c5be03b9df1ebb
l3alisidahmed <sidoabdelali06@gmail.com>
2024-04-13 23:06:14 +0100

    change style

--- client/src/components/Nav Bar/NavBar.jsx
+++ client/src/components/Nav Bar/NavBar.jsx
@@ -1,15 +1,29 @@
-import { Grid, GridItem, Wrap, WrapItem, Image, Heading, HStack, Button } from '@chakra-ui/react'
+'use client';
+
+import { Grid, GridItem, HStack, Button } from '@chakra-ui/react'
 import Link from 'next/link'
 import Logo from '../Logo/Logo';
+import { IoMdSearch } from "react-icons/io";
+import { useRef } from 'react';
 
 const NavBar = () => {
+
+    const inputRef = useRef();
+
+    const onFocus = () => {
+        if(inputRef.current) {
+            inputRef.current.style.outline = 'none';
+            console.log(inputRef.current.style);
+        }
+    }
+
     return(
         <Grid templateColumns='repeat(12, 1fr)' gap={6} bg='black' color='white' p={4} alignItems='center'>
             <GridItem colSpan={2}>
                 <Logo />          
             </GridItem>
-            <GridItem colSpan={5} alignItems='center' justifyContent='center'>
-                <HStack spacing={4}>
+            <GridItem colSpan={5} alignItems='center'>
+                <HStack spacing={4} justifyContent='center'>
                     <Link href='/'>Home</Link>
                     <Link href='/'>About</Link>
                     <Link href='/'>Courses</Link>
@@ -17,13 +31,13 @@ const NavBar = () => {
                 </HStack>
             </GridItem>
             <GridItem colSpan={3}>
-                <HStack spacing='24px'>
-                    <input type="search" name="" id="" style={{color: 'black', paddingLeft: '10px', paddingBlock: '5px', width: '100%'}} />
+                <HStack spacing='5px' bgColor='white' paddingLeft='10' p='1' borderRadius='8'>
+                    <IoMdSearch color='black' size='30px' />
+                    <input ref={inputRef} type="search" name="" id="" style={{color: 'black', paddingLeft: '10px', paddingBlock: '5px', width: '100%', backgroundColor: 'transparent'}} onFocus={onFocus} />
                 </HStack>
             </GridItem>
-            <GridItem colSpan={2}>
-                <Button colorScheme='blue' variant='solid'>Login</Button>
-                <Button colorScheme='green' variant='solid'>Register</Button>
+            <GridItem colSpan={2} display='grid' justifyItems='center'> 
+            <Button bgColor='#234C51' color='white' w='60%' variant='solid' _hover={{bgColor: 'transparent', border: '1px solid #234C51', transition: 'background-color 0.5s ease, border 0.5s ease'}}>Login</Button>
             </GridItem>
         </Grid>
     );
