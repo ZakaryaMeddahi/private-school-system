3c3d853d6dfc600c2873797e4f8cc91beedecbda
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-04-27 17:26:56 +0100

    Add leave session functionality

--- client/src/components/SessionComponents/LeaveButton.jsx
+++ client/src/components/SessionComponents/LeaveButton.jsx
@@ -1,7 +1,28 @@
+import { StreamingContext } from '@/Pages/Session';
 import { Center } from '@chakra-ui/react';
+import { useContext } from 'react';
 import { MdOutlineCall } from 'react-icons/md';
 
 function LeaveButton() {
+  const value = useContext(StreamingContext);
+  
+  const {
+    localCameraTrackRef,
+    localScreenTrackRef,
+    localAudioTrackRef,
+    clientRef,
+  } = value;
+
+  const endCall = async () => {
+    localCameraTrackRef.current?.close();
+    localCameraTrackRef.current = null;
+    localScreenTrackRef.current?.close();
+    localScreenTrackRef.current = null;
+    localAudioTrackRef.current?.close();
+    localAudioTrackRef.current = null;
+    clientRef.current.leave();
+  };
+
   return (
     <Center
       w='50px'
@@ -9,6 +30,7 @@ function LeaveButton() {
       borderRadius='50px'
       bgColor='#F95252'
       cursor='pointer'
+      onClick={endCall}
     >
       <MdOutlineCall size='30px' />
     </Center>
