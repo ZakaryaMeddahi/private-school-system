75b8f982e09218f2605cf8a9c10460bb32c925e2
l3alisidahmed <sidoabdelali06@gmail.com>
2024-04-28 01:25:14 +0100

    change style of course card

--- client/src/components/CardForCourse.jsx
+++ client/src/components/CardForCourse.jsx
@@ -13,8 +13,11 @@ const CardForCourse = ({ course }) => {
             gap={3}
         >
             <Box>
-                <Text fontWeight={700} fontSize={20}>Web Developoment</Text>
-                <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam suscipit at nihil veritatis quo, atque nemo</Text>
+                <Text fontWeight={700} fontSize={20} color='#213E69'>Web Developoment</Text>
+                <Text color='#898C81'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Laboriosam suscipit at nihil veritatis quo, atque nemo
+                </Text>
             </Box>
             <Box
                 gap={5}
