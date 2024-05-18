086b81d36d3d7f4f637cf5d14ab37c436a4c5339
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-05-18 08:23:41 +0100

    add enrollment template

--- server/src/modules/mail/mail.service.ts
+++ server/src/modules/mail/mail.service.ts
@@ -1,6 +1,8 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { HttpException, Injectable } from '@nestjs/common';
+import { Course } from 'src/shared/entities/course.entity';
 import { User } from 'src/shared/entities/user.entity';
+import { EnrollmentStatus } from 'src/shared/enums';
 
 @Injectable()
 export class MailService {
@@ -14,6 +16,29 @@ export class MailService {
         template: './registration',
         context: {
           user,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Cannot send email', 500);
+    }
+  }
+
+  async sendEnrollmentStatus(
+    user: User,
+    status: EnrollmentStatus,
+    course: Course,
+    subject: string,
+  ) {
+    try {
+      await this.mailerService.sendMail({
+        to: user.email,
+        subject,
+        template: './enrollment-status',
+        context: {
+          user,
+          status,
+          course,
         },
       });
     } catch (error) {
