d4b16fc518cc3007e60b80361c3a4e861484f73b
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-06-10 20:33:31 +0100

    remove blanc page that shows in dashboard

--- client/src/app/(Admin)/admin_dashboard/page.jsx
+++ client/src/app/(Admin)/admin_dashboard/page.jsx
@@ -1,6 +1,11 @@
+'use client'
+
 import Overview from "@/Pages/Overview";
+import { useRouter } from "next/navigation";
 
 const Admin = () => {
+    const router = useRouter()
+    router.replace('/admin_dashboard/teachers')
     return (
         <></>
         // <Overview />
--- client/src/app/(Teacher)/teacher_dashboard/page.jsx
+++ client/src/app/(Teacher)/teacher_dashboard/page.jsx
@@ -1,6 +1,11 @@
+'use client'
+
 import Overview from "@/Pages/Overview"
+import { useRouter } from "next/navigation"
 
 const teacher_dashboard = () => {
+    const router = useRouter()
+    router.replace('/teacher_dashboard/my_courses')
     return (
         <></>
         // <Overview />
